2f0f75e826946fd19da6da773d6cc2c392d76047
Jared <jbates9210@gmail.com>
2018-04-02 17:51:30 -0400

    initial commit with table created and placeholders

--- TimesTables/ViewController.swift
+++ TimesTables/ViewController.swift
@@ -8,8 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var table: UITableView!
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "Hi!"
+        return cell
+    }
+    
+    @IBAction func slideChange(_ sender: Any) {
+        print(slider.value)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
